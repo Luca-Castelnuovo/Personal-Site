b60008bd95f4f18fd1da14d05dce2d06ce5f34dc
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-24 20:19:44 +0200

    e

--- js/main.js
+++ js/main.js
@@ -14,3 +14,29 @@ $(function(){$(".page-scroll a").bind("click",function(o){var l=$(this);$("html,
 
 //contact
 $(function(){$("input,textarea").jqBootstrapValidation({preventSubmit:!0,submitError:function(t,i,n){},filter:function(){return $(this).is(":visible")}}),$('a[data-toggle="tab"]').click(function(t){t.preventDefault(),$(this).tab("show")})}),$("#name").focus(function(){$("#success").html("")});
+
+$(document).ready(function() {
+    $('#link').on('click', function(e) {
+        smooth_scroll_to(this.hash, e);
+    });
+});
+
+$(window).on('load',function() {
+    smooth_scroll_to(window.location.hash);
+});
+
+function smooth_scroll_to(hash, e) {
+    if(hash === '') {
+        return false;
+    } else {
+        if($(hash).length > 0) {
+            if(typeof e !== 'undefined') {
+                e.preventDefault();
+                history.pushState(null, null, $(e.target).attr('href'));
+            }
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - $('#first-element').height() - $('#second-element').height()
+            }, 350);
+        }
+    }
+}
