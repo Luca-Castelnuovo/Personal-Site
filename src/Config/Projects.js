ae1dc4580c24513785763a19c1c47f4f53da59e7
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-11-15 14:48:14 +0100

    update

--- src/Config/Projects.js
+++ src/Config/Projects.js
@@ -10,56 +10,10 @@ import broodtNuBackend from 'assets/images/broodtnu.png';
 
 const Projects = [
     {
-        title: 'A.N.D. Aannemersbedrijf',
-        image: andAannemersbedrijf,
-        description: 'Description',
-        tags: ['tag1', 'tag2'],
-        github_link:
-            'https://github.com/Luca-Castelnuovo/A.N.D.-Aannemersbedrijf',
-        demo_link: 'http://and-aannemersbedrijf.nl/'
-    },
-    {
-        title: 'BVH Bouw en Timmerwerken',
-        image: bvhBouwEnTimmerwerken,
-        description: 'Description',
-        tags: ['tag1', 'tag2'],
-        github_link:
-            'https://github.com/Luca-Castelnuovo/BVH-Bouw-en-Timmerwerken',
-        demo_link: 'https://bvhbouwentimmerwerken.nl/'
-    },
-    {
-        title: 'HBL: Kerstmarkt',
-        image: hblKerstmarkt,
-        description: 'Description',
-        tags: ['tag1', 'tag2']
-    },
-    {
-        title: 'HBL: Lustrum',
-        image: hblLustrum,
-        description: 'Description',
-        tags: ['tag1', 'tag2']
-    },
-    {
-        title: 'HBL: BetaSterren',
-        image: hblBetaSterren,
-        description: 'Description',
-        tags: ['tag1', 'tag2'],
-        github_link: 'https://github.com/Luca-Castelnuovo/BetaPlus',
-        demo_link: 'https://betasterren.hetbaarnschlyceum.nl'
-    },
-    {
-        title: 'Centrum Internationale Kinderontvoering',
-        image: centrumInternationaleKinderontvoering,
-        description: 'Description',
-        tags: ['tag1', 'tag2'],
-        demo_link:
-            'https://www.kinderontvoering.org/uploads/stappenplan/index.html'
-    },
-    {
         title: 'Het Hak Hout',
         image: hetHakHout,
         description: 'Description',
-        tags: ['tag1', 'tag2'],
+        tags: ['REACT', 'HTML', 'SASS'],
         github_link: 'https://github.com/Luca-Castelnuovo/Het-Hak-Hout',
         demo_link: 'https://hethakhout.nl'
     },
@@ -67,16 +21,69 @@ const Projects = [
         title: 'WiskundeSite',
         image: wiskundeSite,
         description: 'Description',
-        tags: ['tag1', 'tag2'],
+        tags: ['PHP', 'LUMEN'],
         github_link: 'https://github.com/Luca-Castelnuovo/WiskundeSite',
         demo_link: 'https://api.wiskundesite.ml'
+    },
+    {
+        title: 'BVH Bouw en Timmerwerken',
+        image: bvhBouwEnTimmerwerken,
+        description: 'Description',
+        tags: ['MaterializeCSS', 'HTML', 'CSS', 'JS'],
+        github_link:
+            'https://github.com/Luca-Castelnuovo/BVH-Bouw-en-Timmerwerken',
+        demo_link: 'https://bvhbouwentimmerwerken.nl/'
+    },
+    {
+        title: 'HBL: BetaSterren',
+        image: hblBetaSterren,
+        description: 'Description',
+        tags: [
+            'PHP',
+            'DATABASE',
+            'AUTHENTICATION',
+            'MaterializeCSS',
+            'HTML',
+            'SASS'
+        ],
+        github_link: 'https://github.com/Luca-Castelnuovo/BetaPlus',
+        demo_link: 'https://betasterren.hetbaarnschlyceum.nl'
+    },
+    {
+        title: 'Centrum Internationale Kinderontvoering',
+        image: centrumInternationaleKinderontvoering,
+        description: 'Description',
+        tags: ['HTML', 'CSS', 'JS'],
+        demo_link:
+            'https://www.kinderontvoering.org/uploads/stappenplan/index.html'
     },
     {
         title: 'BroodtNu Backend',
         image: broodtNuBackend,
         description: 'Description',
-        tags: ['tag1', 'tag2'],
+        tags: ['PHP', 'LUMEN'],
         github_link: 'https://github.com/broodt-nu/backend'
+    },
+    {
+        title: 'A.N.D. Aannemersbedrijf',
+        image: andAannemersbedrijf,
+        description: 'Description',
+        tags: ['MaterializeCSS', 'HTML', 'CSS', 'JS'],
+        github_link:
+            'https://github.com/Luca-Castelnuovo/A.N.D.-Aannemersbedrijf',
+        demo_link: 'http://and-aannemersbedrijf.nl/'
+    },
+    {
+        title: 'HBL: Kerstmarkt',
+        image: hblKerstmarkt,
+        description: 'Description',
+        tags: ['PHP', 'WordPress']
+    },
+    {
+        title: 'HBL: Lustrum',
+        image: hblLustrum,
+        description: 'Description',
+        tags: ['PHP', 'WordPress']
     }
 ];
 
