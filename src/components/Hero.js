ab726fe80a9f742b48bc9310b71a541936e50d17
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-11-19 12:13:04 +0100

    updated title

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
                     <p className="subtitle">Hi,</p>
                     <h1 className="title has-text-white">I'm Luca</h1>
                     <h2 className="subtitle has-text-white">
-                        A Back-End Developer
+                        A Back-End Developer and Ethical-Hacker
                     </h2>
                 </div>
             </div>
