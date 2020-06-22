d1d1516217c9b1a873d11f2f20ee3b6a418940a7
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-06-22 14:33:56 +0200

    remove old analytics

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Analytics from 'components/Analytics';
 
 import Home from 'routes/Home';
 import Projects from 'routes/Projects';
@@ -22,7 +21,6 @@ const App = () => {
 
     return (
         <Router>
-            <Analytics />
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/projects" component={Projects} />
